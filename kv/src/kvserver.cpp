f214557ae183e7ec23063184c5014614d705ce02
vvz17 <81757204+vvz17@users.noreply.github.com>
2021-04-07 20:59:22 +0300

    Update kvserver.cpp

--- kv/src/kvserver.cpp
+++ kv/src/kvserver.cpp
@@ -1,9 +1,40 @@
 //============================================================================
 // Name        : kvserver.cpp
-// Author      : vvz17
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Author      : Василий Зазнобин
+// Version     : 1.0
+// Description : Реализация небольшой key-value базы данных.
+
+// Cервер слушает запросы на указанном IP:PORT и предоставляет API для простой key-value базы данных.
+// Внутреннее хранилище сервера - boost::multi_index, который хранится в маппируемом в оперативную память файле.
+// Ключ имеет тип - Строка (максимальная длина 1024 символа).
+// Значение имеет тип - Строка (максимальная длина 1024 * 1024 символа).
+
+// База данных поддерживает операции:
+//    INSERT - добавить key:value;
+//    UPDATE - изменить key:value;
+//    DELETE - удалить key;
+//    GET - получить value по key.
+
+// Если ключ уже существует, то при операции INSERT База Данных возвращает ошибку, что запись отсутсвует.
+// Если ключ не существует, то при операции UPDATE База Данных возвращает ошибку, что запись отсутсвует.
+// Если ключ существует и значение совпадает, то при операции UPDATE База Данных возвращает ошибку, что значение не было изменено.
+// Если ключ не существует, то при операции DELETE База Данных возвращает ошибку об отсутствующей записи.
+// Если ключ не существует, то при операции GET База Данных возвращает ошибку об отсутствующей записи.
+
+// Сервер ведет статистику отправленных и полученных комманд.
+// С переодичности в 60 секунд, сервер выводит на std::cerr статистику:
+//    - количество записей в БД;
+//    - количество успешных/неуспешных операций INSERT;
+//    - количество успешных/неуспешных операций UPDATE;
+//    - количество успешных/неуспешных операций DELETE;
+//    - количество успешных/неуспешных операций GET.
+
+// Общение по собственному протоколу поверх TCP/IP.
+// Для автоматизации сериализации структур использовать boost::fusion (не реализовано).
+// Для реализации сетевого взаимодействия использовать boost::asio.
+// Для реализации потоков использовать возможности boost::asio.
+// Для реализации таймеров использовать возможности boost::asio.
+// Для хранения данных использовать boost::multi_index и boost::interprocess.
 //============================================================================
 
 #include <iostream>
@@ -16,6 +47,14 @@
 #include <memory>
 #include <utility>
 #include <thread>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/utility.hpp>
 
 #include <boost/version.hpp>
 #include <boost/container/flat_map.hpp>
@@ -37,11 +76,11 @@
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/read_until.hpp>
 
-const std::string INSERT = "INSERT";
-const std::string UPDATE = "UPDATE";
-const std::string DELETE = "DELETE";
-const std::string GET = "GET";
-const char delim = ',';
+constexpr const char* INSERT = "INSERT";
+constexpr const char* UPDATE = "UPDATE";
+constexpr const char* DELETE = "DELETE";
+constexpr const char* GET = "GET";
+constexpr char delim = ',';
 
 namespace bip = boost::interprocess;
 using mutex_type    = bip::named_mutex;
@@ -125,6 +164,14 @@ static multi_index_kv_t& shared_instance2()
     return *_instance;
 }
 
+struct stat_t
+{
+	std::pair<int,int> INSERT;
+	std::pair<int,int> UPDATE;
+	std::pair<int,int> DELETE;
+	std::pair<int,int> GET;
+};
+
 template<typename KV>
 struct cmd_t : public key_value_t
 {
@@ -149,22 +196,22 @@ struct cmd_t : public key_value_t
 		value.assign(buf.begin(), buf.end());
 	}
 
-	std::string handle();
+	std::string handle(stat_t&);
 };
 
 template<>
-	std::string cmd_t<multi_index_kv_t>::handle()
+	std::string cmd_t<multi_index_kv_t>::handle(stat_t& stat)
 	{
 		std::stringstream ss;
 
 		if(name == INSERT)
 		{
-//			std::clog << "start handling " << INSERT << std::endl;
 			auto &index = kv.get<key_value_t::by_key>();
 			auto it = index.find(key);
 			if(it != index.end())
 			{
 				ss << name << " ERROR key exists: " << key << std::endl;
+				++stat.INSERT.second;
 				return ss.str();
 			}
 
@@ -172,69 +219,75 @@ template<>
 			if(it2.second)
 			{
 				ss << name << " OK new key-value: " << (*it2.first).key << '=' << (*it2.first).value << std::endl;
+				++stat.INSERT.first;
 				return ss.str();
 			}
 		}
 		else if(name == UPDATE)
 		{
-//			std::clog << "start handling " << UPDATE << std::endl;
 			auto &index = kv.get<key_value_t::by_key>();
 			auto it = index.find(key);
 			if(it == index.end())
 			{
 				ss << name << " ERROR key not found: " << key << std::endl;
+				++stat.UPDATE.second;
 				return ss.str();
 			}
 			if((*it).value == value)
 			{
 				ss << name << " ERROR value has not been changed: " << key << '=' << (*it).value << std::endl;
+				++stat.UPDATE.second;
 				return ss.str();
 			}
 
 			index.modify(it, [this](key_value_t &e){ e.value = value; });
 			ss << name << " OK new value: " << (*it).key << '=' << (*it).value << std::endl;
+			++stat.UPDATE.first;
 			return ss.str();
 		}
 		else if(name == DELETE)
 		{
-//			std::clog << "start handling " << DELETE << std::endl;
 			auto &index = kv.get<key_value_t::by_key>();
 			auto it = index.find(key);
 			if(it == index.end())
 			{
 				ss << name << " ERROR key not found: " << key << std::endl;
+				++stat.DELETE.second;
 				return ss.str();
 			}
 
 			index.erase(it);
 
 			ss << name << " OK key #count: " << key << "#" << kv.count(key) << std::endl;
+			++stat.DELETE.first;
 			return ss.str();
 		}
 		else if(name == GET)
 		{
-//			std::clog << "start handling " << GET << std::endl;
 			auto &index = kv.get<key_value_t::by_key>();
 			auto it = index.find(key);
 			if(it == index.end())
 			{
 				ss << name << " ERROR key not found: " << key << std::endl;
+				++stat.GET.second;
 				return ss.str();
 			}
 
 			ss << name << " OK key found: " << key << '=' << (*it).value << std::endl;
+			++stat.GET.first;
 			return ss.str();
 		}
 		else
 		{
 			ss << name << " ERROR unknown command" << std::endl;
+			++stat.GET.second;
 			return ss.str();
 		}
 		return ss.str();
 	}
 
 template<>
-	std::string cmd_t<kv_t>::handle()
+	std::string cmd_t<kv_t>::handle(stat_t&)
 	{
 		std::stringstream ss;
 		if(name == INSERT)
@@ -322,9 +375,14 @@ template<typename KV>
 class session
 {
 public:
-	session(KV& kv, boost::asio::io_service& io_service)
+	session( KV& kv
+			, boost::asio::io_service& io_service
+			, stat_t& stat
+			, boost::shared_mutex& mx)
 	: kv_(kv)
 	, socket_(io_service)
+	, stat_(stat)
+	, mx_(mx)
 	{
 	}
 
@@ -351,9 +409,13 @@ private:
 			std::istream is(&receive_data);
 			std::getline(is, line);
 
-//			std::clog << "received: " << line << std::endl;
 			auto cmd = parse_cmd(kv_,line);
-			std::string answer = cmd.handle();
+			std::string answer;
+
+			{
+				boost::lock_guard<boost::shared_mutex> guard(mx_);
+				answer = cmd.handle(stat_);
+			}
 
 			std::ostream os(&send_data);
 			os << answer << std::endl;
@@ -386,6 +448,8 @@ private:
 
 	KV& kv_;
 	tcp::socket socket_;
+	stat_t& stat_;
+	boost::shared_mutex& mx_;
 	boost::asio::streambuf send_data;
 	boost::asio::streambuf receive_data;
 };
@@ -394,32 +458,33 @@ template<typename KV>
 class server
 {
 public:
-	server(KV& kv, boost::asio::io_service& io_service, short port)
+	server(KV& kv, boost::asio::io_service& io_service, short port, long seconds_on_time_statistics)
 	: kv_(kv)
-	, io_service_(io_service),
-	  acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+	, io_service_(io_service)
+	, acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+	, interval(seconds_on_time_statistics)
+	, timer(io_service, interval)
 	{
 		start_accept();
 	}
 	void run()
 	{
-	  // Create a pool of threads to run all of the io_contexts.
 	  std::vector<boost::shared_ptr<std::thread> > threads;
 	  for (std::size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
 	  {
-	    boost::shared_ptr<std::thread> thread(new std::thread(
-	          boost::bind(&boost::asio::io_service::run, &io_service_)));
+	    boost::shared_ptr<std::thread> thread(new std::thread(boost::bind(&boost::asio::io_service::run, &io_service_)));
 	    threads.push_back(thread);
 	  }
 
-	  // Wait for all threads in the pool to exit.
+	  timer.async_wait(boost::bind(&server<KV>::on_time_statistic, this, _1));
+
 	  for (std::size_t i = 0; i < threads.size(); ++i)
 	    threads[i]->join();
 	}
 private:
 	void start_accept()
 	{
-		session<KV>* new_session = new session<KV>(kv_,io_service_);
+		session<KV>* new_session = new session<KV>(kv_,io_service_,stat_,mx_);
 		acceptor_.async_accept(new_session->socket(),
 				boost::bind(&server::handle_accept, this, new_session,
 						boost::asio::placeholders::error));
@@ -439,24 +504,40 @@ private:
 
 		start_accept();
 	}
+	void on_time_statistic(const boost::system::error_code& /*e*/) {
+
+	    boost::lock_guard<boost::shared_mutex> guard(mx_);
+
+	    std::cout << "------" << std::endl;
+	    std::cout << "count  " << kv_.size() << std::endl;
+	    std::cout << "INSERT " << stat_.INSERT.first << "/" << stat_.INSERT.second << std::endl;
+	    std::cout << "UPDATE " << stat_.UPDATE.first << "/" << stat_.UPDATE.second << std::endl;
+	    std::cout << "DELETE " << stat_.DELETE.first << "/" << stat_.DELETE.second << std::endl;
+	    std::cout << "GET    " << stat_.GET.first << "/" << stat_.GET.second << std::endl;
+
+	    timer.expires_at(timer.expires_at() + interval);
+	    timer.async_wait(boost::bind(&server<KV>::on_time_statistic, this, _1));
+	}
 
 	KV& kv_;
 	boost::asio::io_service& io_service_;
 	tcp::acceptor acceptor_;
+	boost::posix_time::seconds interval;
+	boost::asio::deadline_timer timer;
+	stat_t stat_;
+	boost::shared_mutex mx_;
 };
 
 int main(int argc, char* argv[]) {
 
 	try
 	{
-		std::cout << "KVServer" << std::endl; // prints KVServer
+		std::cout << "kvserver" << std::endl;
 
 		if (argc != 2) {
-			std::cerr << "Usage: async_db_server <port>\n";
+			std::cerr << "Usage: kvserver <port>\n";
 			return 1;
 		}
-
-		std::clog << "KV" << argc << std::endl;
 
 //		auto& kv = shared_instance();
 		auto& kv = shared_instance2();
@@ -464,7 +545,7 @@ int main(int argc, char* argv[]) {
 		bip::scoped_lock<mutex_type> lock(mutex);
 
 		boost::asio::io_service io_service;
-		server<decltype(kv)> s(kv, io_service, std::atoi(argv[1]));
+		server<decltype(kv)> s(kv, io_service, std::atoi(argv[1]), 6);
 
 		s.run();
 	}
